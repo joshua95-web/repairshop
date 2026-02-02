99da5723aedc907e0d3389ad09059487293ba607
joshua95-web <joshcscurfield@gmail.com>
2026-02-02 16:29:11 +0000

    made grouped directory for employee portal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="bg-black bg-[url(/therepairshop.png)] bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
-          <h1>
+          <h1 className="text-4xl font-bold">
             The Computer
             <br />
             Repair Shop
