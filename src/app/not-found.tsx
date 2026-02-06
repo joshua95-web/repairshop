1788853b6b1b8884c0357985323fa0a600a40b53
joshua95-web <joshcscurfield@gmail.com>
2026-02-06 14:17:05 +0000

    added global and local error page + 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "404: Page Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h2 className="font-mono font-extrabold text-blue-300 bg-red-800 px-4 py-2 rounded">
+        404 Not Found
+      </h2>
+      <Image
+        className="m-0 rounded-xl"
+        src="/404.png"
+        alt="404 not found"
+        width={600}
+        height={800}
+        priority={true}
+        title="Page Not Found"
+      />
+    </div>
+  );
+}
